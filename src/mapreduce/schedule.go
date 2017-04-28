35cf8372c63bfa43fb1bd179ccde74254ede7d36
CharlieFeng <fsy19920220@gmail.com>
2017-04-28 11:23:18 +1000

    Lab1 PartIV Done

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -29,49 +29,50 @@ func (mr *Master) schedule(phase jobPhase) {
 
 
 	fmt.Printf("Now in %s phase, total %d tasks\n", phase, ntasks)
+
+	var execute func(worker string, taskNum int, phase jobPhase)
+	execute = func( worker string, taskNum int, phase jobPhase){
+		fmt.Printf("Worker %s will execute task %d on phase %s\n", worker, taskNum, phase)
+		taskArgs := new(DoTaskArgs)
+		taskArgs.JobName = mr.jobName
+		taskArgs.File = mr.files[taskNum]
+		taskArgs.Phase = phase
+		taskArgs.TaskNumber = taskNum
+		taskArgs.NumOtherPhase = nios
+
+		ok := call(worker, "Worker.DoTask", &taskArgs, new(struct{}))
+		if ok {
+			mr.registerChannel <- worker
+			fmt.Printf("Worker %s finish %d task in %v phase\n", worker, taskNum, phase)
+		}else{
+			fmt.Println("Catch worker failure")
+			fmt.Println("Current workers in Master", mr.workers)
+			for i, w := range mr.workers {
+				if w == worker {
+					mr.workers = append(mr.workers[:i], mr.workers[i+1:]...)
+					break
+				}
+			}
+			fmt.Println("After,  workers in Master", mr.workers)
+			var w string
+			if len(mr.workers) > 0 {
+				fmt.Printf("Master current workers number %d\n", len(mr.workers))
+				w = mr.workers[0]
+
+			}else{
+				fmt.Printf("No workers right now, has to wait", len(mr.workers))
+				w = <-mr.registerChannel
+			}
+			execute(w, taskNum, phase)
+		}
+	}
+
 	for taskNum:=0; taskNum<ntasks; taskNum++{
 
 		var worker string
 		worker = <-mr.registerChannel
-		go func( worker string, taskNum int, phase jobPhase){
-			fmt.Printf("Worker %s will execute task %d on phase %s\n", worker, taskNum, phase)
-			taskArgs := new(DoTaskArgs)
-			taskArgs.JobName = mr.jobName
-			taskArgs.File = mr.files[taskNum]
-			taskArgs.Phase = phase
-			taskArgs.TaskNumber = taskNum
-			taskArgs.NumOtherPhase = nios
-
-			ok := call(worker, "Worker.DoTask", &taskArgs, new(struct{}))
-			if ok {
-				mr.registerChannel <- worker
-				fmt.Printf("Worker %s finish %d task in %v phase\n", worker, taskNum, phase)
-			}else{
-				fmt.Println("Catch worker failure")
-				fmt.Println("Current workers in Master", mr.workers)
-				for i, w := range mr.workers {
-					if w == worker {
-						mr.workers = append(mr.workers[:i], mr.workers[i+1:]...)
-						break
-					}
-				}
-				fmt.Println("After,  workers in Master", mr.workers)
-				var w string
-				if len(mr.workers) > 0 {
-					fmt.Printf("Master current workers number %d\n", len(mr.workers))
-					w = mr.workers[0]
-
-				}else{
-					fmt.Printf("No workers right now, has to wait", len(mr.workers))
-					w = <-mr.registerChannel
-				}
-				ok := call(w, "Worker.DoTask", &taskArgs, new(struct{}))
-				if ok {
-					fmt.Printf("Worker %s finish %d task in %v phase\n", w, taskNum, phase)
-					mr.registerChannel <- w
-				}
-			}
-		}(worker,taskNum, phase)
+		
+		go execute(worker, taskNum, phase)
 
 	}
 
